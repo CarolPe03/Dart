554933023720d198caaa3dd44e49dfe29c538c14
CarolPe03 <anacarolpc11@gmail.com>
2023-01-27 11:24:44 -0600

    Clase abstracta

--- clases/dart_application_1/bin/dart_application_1.dart
+++ clases/dart_application_1/bin/dart_application_1.dart
@@ -1,15 +1,33 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
   var unaLaptop= Computadora(marca: 'Dell',cpu: 'Intel core i9',ram:32);
   unaLaptop.marca='Lenovo';
   unaLaptop.cpu='Celeron';
   unaLaptop.ram=4;
   print('Información de la Laptop que te llegó a cada: $unaLaptop');
+  stdout.write('Esta computadora es adecuada para la materia DMM?: ');
+  
+  String? respuesta=stdin.readLineSync();
+  if(respuesta !=null){
+    if(respuesta.contains('si')){
+    unaLaptop.configurarAdecuada=true;
+    } else {
+    unaLaptop.configurarAdecuada=false;
+    } 
+    }else {
+    print('No se puede saber si la PC es adecuada porque se recibió null');
+  }
+  print('Laptop1 es adecuada para tu materia: ${unaLaptop.esAdecuada}');
+unaLaptop.marca='Lenovo';
 }
+
 class Computadora{
   //Campos
   String? marca;
   String? cpu;
   int? ram;
+  bool adecuada=false;
 
  /* Computadora(String m,String c, int r){
     this.marca= m;
@@ -22,6 +40,15 @@ class Computadora{
   @override
   String toString() {
     // TODO: implement toString
-    return 'Marca: ${this.marca}, CPU: ${this.cpu}, RAM: ${this.ram}';
+    return 'Marca: $marca, CPU: $cpu, RAM: $ram,es adecuada para DMM: $adecuada';
   }
+  //getter de la variable adecuada
+  bool get esAdecuada{
+    return adecuada;
+  }
+  //setter de la variable adecuada 
+  set configurarAdecuada(bool valor){
+    adecuada = valor;
+  }
+
 }
